1f0551c095b68ffbe11e61ab0c2a055a7fcc15bc
sultan2110 <sultantanirergenov@gmail.com>
2025-02-23 15:00:39 +0500

    Ertalabki tayin boldi

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,75 +1,55 @@
-import React, { useState } from 'react'
-import Sidebar from '../../components/Sidebar/Sidebar'
-import Navbar from '../../components/Navbar/Navbar'
-import Table from './Table'
-import Modal from './Modal'
+import React, { useState } from "react";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Navbar from "../../components/Navbar/Navbar";
+import Table from "./Table";
+import Modal from "./Modal";
 import { BsTaxiFront } from "react-icons/bs";
-import "./Home.css"
+import "./Home.css";
 
 function Home() {
-    const [modalOpen, setmodalOpen] = useState(false);
-    const [formType, setFormType] = useState(null);
-    const [rows, setRows] = useState([]);
-    const [currentRow, setCurrentRow] = useState({});  // Yangi to'liq qatorni saqlash
-    const [rowedit, setRowEdit] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [rows, setRows] = useState([]);
 
-    const handleRowEdit = (idx) => {
-        setRowEdit(idx);
-        setmodalOpen(true);
-        setFormType("form1");
-    };
+  const handleDelete = () => {};
 
-    const handleDelete = (idx) => {
-        setRows(rows.filter((_, i) => i !== idx));
-    };
+  const handleRowEdit = () => {};
 
-    const handleSubmit = (newRow) => {
-        const updatedRow = { ...currentRow, ...newRow };
-
-        // Agar barcha ma'lumotlar kiritilgan bo'lsa, yangi qatorni qo'shamiz
-        if (updatedRow.sana && updatedRow.ertalabkispid && updatedRow.kunyakunspid && updatedRow.quyilganyoqilgi) {
-            if (rowedit !== null) {
-                // To'liq qatorni yangilash
-                setRows(rows.map((row, idx) => (idx === rowedit ? updatedRow : row)));
-            } else {
-                // Yangi qatorni saqlash
-                setRows([...rows, updatedRow]);
-            }
-            setmodalOpen(false);  // Modalni yopamiz
-            setCurrentRow({});  // Yangi qatorni tozalaymiz
-        } else {
-            // Hali to'liq ma'lumotlar kiritilmagan bo'lsa, yangi rowni yangilash
-            setCurrentRow(updatedRow);
-        }
-    };
-
-    return (
-        <div className='home'>
-            <Sidebar />
-            <div className='main'>
-                <Navbar />
-                <div className='addandavto'>
-                    <div className='iconavto'>
-                        <span><BsTaxiFront /></span>
-                    </div>
-                    <div><span onClick={() => { setmodalOpen(true); setFormType("form1"); }}>Ertalabki</span></div>
-                    <div><span onClick={() => { setmodalOpen(true); setFormType("form2"); }}>Kun yakuni</span></div>
-                    <div><span onClick={() => { setmodalOpen(true); setFormType("form3"); }}>Zapravka</span></div>
-                </div>
-
-                <Table rows={rows} deleteRow={handleDelete} editRow={handleRowEdit} />
-
-                {modalOpen && (
-                    <Modal
-                        closeModal={() => setmodalOpen(false)}
-                        onSubmit={handleSubmit}
-                        defaultValue={rowedit !== null ? rows[rowedit] : {}}
-                        formType={formType} // formType ma'lumotini yuboramiz
-                    />
-                )}
-            </div>
+  console.log(modalOpen);
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="main">
+        <Navbar />
+        <div className="addandavto">
+          <div className="iconavto">
+            <span>
+              <BsTaxiFront />
+            </span>
+          </div>
+          <div>
+            <span onClick={() => setModalOpen(true)}>Ertalabki</span>
+          </div>
+          <div>
+            <span onClick={() => setModalOpen(true)}>Kun yakuni</span>
+          </div>
+          <div>
+            <span onClick={() => setModalOpen(true)}>Zapravka</span>
+          </div>
         </div>
-    );
+
+        <Table rows={rows} deleteRow={handleDelete} editRow={handleRowEdit} />
+
+        {modalOpen && (
+          <Modal
+            closeModal={setModalOpen}
+            rows={rows}
+            setRows={setRows}
+            // formType ma'lumotini yuboramiz
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- src/pages/Home/Modal.jsx
+++ src/pages/Home/Modal.jsx
@@ -1,118 +1,69 @@
-import React, { useState } from 'react'
-import "./Modal.css"
+import React, { useState } from "react";
+import "./Modal.css";
 
-function Modal({ closeModal, onSubmit, defaultValue, formType }) {
+function Modal({ closeModal, rows, setRows }) {
+  const [formState, setFormState] = useState({
+    sana: "",
+    ertalabkispid: "",
+    ertalabkiqabulbakqoldiq: "",
+    status: "live",
+  });
 
-    const [formState, setFormState] = useState(defaultValue || {
-        sana: "", ertalabkispid: "", ertalabkiqabulbakqoldiq: "", status: "live", kunyakunspid: "", quyilganyoqilgi: ""
+  const handleChange = (e) => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
     });
+  };
 
-    const [errors, setErrors] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const validateForm = () => {
-        if (formState.sana && formState.ertalabkispid) {
-            setErrors("");
-            return true;
-        } else {
-            let errorFields = [];
-            for (const [key, value] of Object.entries(formState)) {
-                if (!value) {
-                    errorFields.push(key);
-                }
-            }
-            setErrors(errorFields.join(","));
-            return false;
-        }
-    };
+    setRows([...rows, formState]);
+    closeModal(false);
+    console.log(formState);
+  };
 
-    const handleChange = (e) => {
-        setFormState({
-            ...formState,
-            [e.target.name]: e.target.value,
-        });
-    };
-
-    const handleFileChange = (e) => {
-        const file = e.target.files[0];
-        setFormState({
-            ...formState,
-            [e.target.name]: file
-        });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!validateForm()) return;
-        onSubmit(formState);
-        closeModal();
-    };
-
-    const renderForm = () => {
-        switch (formType) {
-            case "form1":
-                return (
-                    <>
-                        <div className='form-group'>
-                            <label htmlFor="sana">Sana</label>
-                            <input type="date" name="sana" onChange={handleChange} value={formState.sana} />
-                        </div>
-                        <div className='form-group'>
-                            <label htmlFor="ertalabkispid">Ertalabki spidometr ko'rsatkishi</label>
-                            <input type="number" name="ertalabkispid" onChange={handleChange} value={formState.ertalabkispid} />
-                        </div>
-                        <div className='form-group'>
-                            <label htmlFor="ertalabkiqabulbakqoldiq">Ertalabki qabul qilishda bakdagi qoldiq</label>
-                            <input type="number" name="ertalabkiqabulbakqoldiq" onChange={handleChange} value={formState.ertalabkiqabulbakqoldiq} />
-                        </div>
-                        <div className='form-group'>
-                            <label htmlFor="status">Status</label>
-                            <select name="status" onChange={handleChange} value={formState.status}>
-                                <option value="live">live</option>
-                                <option value="error">error</option>
-                                <option value="draft">draft</option>
-                            </select>
-                        </div>
-                    </>
-                );
-            case "form2":
-                return (
-                    <>
-                        <div className='form-group'>
-                            <label htmlFor="kunyakunspid">Kun yakuni spidometr ko'rsatkishi</label>
-                            <input type="number" name="kunyakunspid" onChange={handleChange} value={formState.kunyakunspid} />
-                        </div>
-                        <div className='form-group'>
-                            <label htmlFor="kunyakunspid">Faylni yuklash</label>
-                            <input type="file" name="kunyakunspid" onChange={handleFileChange} />
-                        </div>
-                    </>
-                );
-            case "form3":
-                return (
-                    <>
-                        <div className='form-group'>
-                            <label htmlFor="quyilganyoqilgi">Quyilgan yoqilgi</label>
-                            <input type="number" name="quyilganyoqilgi" onChange={handleChange} value={formState.quyilganyoqilgi} />
-                        </div>
-                    </>
-                );
-            default:
-                return null;
-        }
-    };
-
-    return (
-        <div className='modal-container' onClick={(e) => { if (e.target.className === "modal-container") closeModal(); }}>
-            <div className='modal'>
-                {errors && <div className="error-messages">{errors}</div>}
-                <form onSubmit={handleSubmit}>
-                    {renderForm()}
-                    <button type="submit" className='btn'>Submit</button>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="modal-container">
+      <div className="modal">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="date"
+            name="sana"
+            value={formState.sana}
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="number"
+            name="ertalabkispid"
+            value={formState.ertalabkispid}
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="number"
+            name="ertalabkiqabulbakqoldiq"
+            value={formState.ertalabkiqabulbakqoldiq}
+            onChange={(e) => handleChange(e)}
+          />
+          <select
+            name="status"
+            id=""
+            value={formState.status}
+            onChange={(e) => handleChange(e)}
+          >
+            <option value="live">Saylan</option>
+            <option value="live">live</option>
+            <option value="error">error</option>
+            <option value="draft">draft</option>
+          </select>
+          <button type="submit" className="btn">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Modal;
- 
